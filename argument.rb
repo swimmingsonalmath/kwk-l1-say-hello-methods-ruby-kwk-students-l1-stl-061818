9481d95970cf8511c97dbe9a800f64a4a3e83a83
Sonali Sharma <sonalisharma58521@gmail.com>
2018-06-26 14:10:51 +0000

    Done.

---
+++ argument.rb
@@ -0,0 +1,12 @@
+def greeting(local_gretting,your_name,my_name,time_of_day)
+  puts"#{local_gretting} #{your_name}, I am #{my_name} and the time is #{time_of_day}"
+end
+
+
+greeting("Hi","Sarah","Sonali","10:00a.m.")
+
+
+
+
+
+
---
+++ morning_routine.rb
@@ -0,0 +1,8 @@
+def morning_routine
+  puts "alarm goes off and i go back to sleep"
+  puts "my secong alarm goes off and I wake up"
+  puts "i get out of bed"
+  puts "make my lunch"
+end
+morning_routine
+morning_routine
